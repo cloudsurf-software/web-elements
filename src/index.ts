e9b3fda774625f00d76ef16a63d13a64306a1dd5
worldMachine <kbdavis77@gmail.com>
2024-09-05 06:22:41 -0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { default as ImageHeader } from "./components/ImageHeader";
 export { default as ImageGallery } from "./components/ImageGallery";
 export { default as ContactForm } from "./components/ContactForm";
 export { default as Nudger } from "./components/Nudger";
+export { default as Features } from "./components/Features";
 
 export * from "./types";
